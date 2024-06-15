9b45f089c1fa860adc211ec38e433ffe96887cea
abhijitpal1247 <abhijit@samasra.net>
2024-06-16 00:17:11 +0530

    fix: resolve issue of incorrect method of passing api key

--- get_lora_weights.py
+++ get_lora_weights.py
@@ -4,8 +4,9 @@ import wget
 
 os.makedirs("cola_weights", exist_ok=True)
 os.makedirs("celebrity_weights", exist_ok=True)
-wget.download(url="https://civitai.com/api/download/models/267807?token=$CIVITAI_API_KEY",
+civitai_api_key = os.getenv('CIVITAI_API_KEY')
+wget.download(url=f"https://civitai.com/api/download/models/267807?token={civitai_api_key}",
               out="cola_weights/pytorch_lora.safetensors")
 
-wget.download(url="https://civitai.com/api/download/models/26680?token=$CIVITAI_API_KEY",
+wget.download(url=f"https://civitai.com/api/download/models/26680?token={civitai_api_key}",
               out="celebrity_weights/pytorch_lora.safetensors")
