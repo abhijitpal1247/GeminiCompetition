d3be4d71cc968ec02fdcb386b44128c7a291f152
abhijitpal1247 <abhijit@samasra.net>
2024-06-16 01:25:38 +0530

    fix: resolve issue with path of extracted files while unzipping

--- download_rife_weights.py
+++ download_rife_weights.py
@@ -6,7 +6,7 @@ os.makedirs("ECCV2022-RIFE-main/train_log/", exist_ok=True)
 gdown.download(id="1APIzVeI-4ZZCEuIRE1m6WYfSCaOsi_7_",
                output="ECCV2022-RIFE-main/train_log/RIFE_trained_model_v3.6.zip")
 with ZipFile("ECCV2022-RIFE-main/train_log/RIFE_trained_model_v3.6.zip", 'r') as zObject:
-    zObject.extractall(path="ECCV2022-RIFE-main/train_log/")
+    zObject.extractall(path="ECCV2022-RIFE-main/")
 
 gdown.download(id="1i3xlKb7ax7Y70khcTcuePi6E7crO_dFc", output="ECCV2022-RIFE-main/demo.mp4")
 
