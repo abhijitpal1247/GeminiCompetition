9b5d559573a9cef8a8addfe903d66580e2a52daa
abhijitpal1247 <abhijit@samasra.net>
2024-06-16 00:23:58 +0530

    fix: replace wget with subprocess calls

--- get_lora_weights.py
+++ get_lora_weights.py
@@ -1,12 +1,13 @@
 import os
-import wget
+import subprocess
 
+import wget
 
 os.makedirs("cola_weights", exist_ok=True)
 os.makedirs("celebrity_weights", exist_ok=True)
-civitai_api_key = os.getenv('CIVITAI_API_KEY')
-wget.download(url=f"https://civitai.com/api/download/models/267807?token={civitai_api_key}",
-              out="cola_weights/pytorch_lora.safetensors")
 
-wget.download(url=f"https://civitai.com/api/download/models/26680?token={civitai_api_key}",
-              out="celebrity_weights/pytorch_lora.safetensors")
+subprocess.run(["wget", "-q", "https://civitai.com/api/download/models/267807?token=$CIVITAI_API_KEY", "-O",
+                "cola_weights/pytorch_lora.safetensors"])
+subprocess.run(["wget", "-q", "https://civitai.com/api/download/models/26680?token=$CIVITAI_API_KEY", "-O",
+                "cola_weights/pytorch_lora.safetensors"])
+
